b29d3d0d25c735114019c75b22442db4cc61a54f
Md Samiul <samiuljust2018@gmail.com>
2025-07-07 17:29:46 +0600

    first commit

---
+++ src/pages/Dashboard/Home.jsx
@@ -0,0 +1,39 @@
+import RecentOrders from "../../components/ecommerce/RecentOrders";
+import PageMeta from "../../components/common/PageMeta";
+import FastCard from "../../components/ecommerce/FirstCard"
+import SliderCard from "../../components/ecommerce/SliderCard"
+import InfoCard from "../../components/ecommerce/InfoCard"
+
+export default function Home() {
+ 
+  return (
+    <>
+      <PageMeta
+        title="Tak Telecom"
+        description=""
+      />
+      <div className="grid grid-cols-12 gap-4 md:gap-6">
+        
+
+        <div className="col-span-12 xl:col-span-7">
+          < FastCard/>
+        </div>
+
+        <div className="col-span-12 xl:col-span-5">
+          <SliderCard />
+        </div>
+
+        <div className="col-span-12 space-y-6 xl:col-span-12">
+        
+          <InfoCard />
+
+        </div>
+
+
+        <div className="col-span-12 xl:col-span-12">
+          <RecentOrders />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/utils/const.js
@@ -0,0 +1,2 @@
+//export const base_url='https://taktelecom-dashboard.milliekit.com/api/reseller'
+export const base_url='https://app-api-vpro-tt.taktelcom.com/api/reseller'
